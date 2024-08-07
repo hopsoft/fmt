2bcc8d55f3e2d2d687bf93384c19337f4bd345c4
Nate Hopkins (hopsoft) <natehop@gmail.com>
2024-08-06 20:15:15 -0600

    Cleanup

--- lib/fmt/sigils.rb
+++ lib/fmt/sigils.rb
@@ -2,7 +2,6 @@
 
 # rbs_inline: enabled
 
-require "forwardable"
 require "monitor"
 require "singleton"
 
@@ -12,56 +11,19 @@ module Fmt
     include MonitorMixin
     include Singleton
 
-    class << self
-      extend Forwardable
+    PREFIX = "%" # :: String -- template prefix
 
-      # The pipe operator
-      # @rbs return: String
-      def_delegator :instance, :pipe_operator
+    KEY_PREFIXES = ["<", "{"].freeze # :: Array[String] -- named-template prefix
 
-      # The embed prefix
-      # @rbs return: String
-      def_delegator :instance, :embed_prefix
+    KEY_SUFFIXES = [">", "}"].freeze # :: Array[String] -- named-template suffix
 
-      # The embed suffix
-      # @rbs return: String
-      def_delegator :instance, :embed_suffix
+    ARGS_PREFIX = "(" # :: String -- macro arguments prefix
 
-      # Changes the pipe operator
-      # @rbs value: String -- new pipe operator
-      # @rbs return: String -- new pipe operator
-      def pipe_operator=(value)
-        synchronize { @pipe_operator = instance.value.to_s }
-      end
+    ARGS_SUFFIX = ")" # :: String -- macro arguments suffix
 
-      # Changes the embed prefix
-      # @rbs value: String -- new embed prefix
-      # @rbs return: String -- new embed prefix
-      def embed_prefix=(value)
-        synchronize { @embed_prefix = instance.value.to_s }
-      end
-
-      # Changes the embed suffix
-      # @rbs value: String -- new embed suffix
-      # @rbs return: String -- new embed suffix
-      def embed_suffix=(value)
-        synchronize { @embed_suffix = instance.value.to_s }
-      end
-    end
-
-    PREFIX = "%" # :: String
-
-    KEY_PREFIXES = ["<", "{"].freeze # :: Array[String]
-
-    KEY_SUFFIXES = [">", "}"].freeze # :: Array[String]
-
-    ARGS_PREFIX = "(" # :: String
-
-    ARGS_SUFFIX = ")" # :: String
-
-    attr_reader :pipe_operator # :: String
-    attr_reader :embed_prefix # :: String
-    attr_reader :embed_suffix # :: String
+    attr_reader :pipe_operator # :: String -- template pipeline operator (i.e. macro delimiter)
+    attr_reader :embed_prefix # :: String -- embeded template prefix
+    attr_reader :embed_suffix # :: String -- embeded template suffix
 
     # Changes the pipe operator
     # @rbs value: String -- new pipe operator
@@ -90,6 +52,11 @@ module Fmt
       @pipe_operator = "|>"
       @embed_prefix = "{{"
       @embed_suffix = "}}"
+    end
+
+    # Expose instance methods on the Fmt::Sigils class
+    public_instance_methods(false).each do |name|
+      define_singleton_method name, ->(*a, **k, &b) { instance.public_send(name, *a, **k, &b) }
     end
   end
 end
